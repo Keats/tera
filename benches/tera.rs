f2f6fc4b9adf9c27775f074e28be153c864c05bb
Vincent Prouillet <vincent@wearewizards.io>
2016-04-04 16:56:57 +0100

    Fix benches

--- benches/tera.rs
+++ benches/tera.rs
@@ -1,5 +1,4 @@
-#![feature(test, custom_derive, plugin)]
-#![plugin(serde_macros)]
+#![feature(test)]
 extern crate test;
 extern crate tera;
 extern crate serde;
@@ -24,7 +23,7 @@ static TEMPLATE: &'static str = "
 </html>
 ";
 
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
 struct Product {
     name: String,
     manufacturer: String,
@@ -38,6 +37,50 @@ impl Product {
             manufacturer: "Motorala".to_owned(),
             summary: "A phone".to_owned(),
             price: 100
+        }
+    }
+}
+// Impl Serialize by hand so tests pass on stable and beta
+impl serde::Serialize for Product {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+        where S: serde::Serializer
+    {
+        serializer.serialize_struct("Product", ProductMapVisitor {
+            value: self,
+            state: 0,
+        })
+    }
+}
+
+struct ProductMapVisitor<'a> {
+    value: &'a Product,
+    state: u8,
+}
+
+impl<'a> serde::ser::MapVisitor for ProductMapVisitor<'a> {
+    fn visit<S>(&mut self, serializer: &mut S) -> Result<Option<()>, S::Error>
+        where S: serde::Serializer
+    {
+        match self.state {
+            0 => {
+                self.state += 1;
+                Ok(Some(try!(serializer.serialize_struct_elt("name", &self.value.name))))
+            },
+            1 => {
+                self.state += 1;
+                Ok(Some(try!(serializer.serialize_struct_elt("manufacturer", &self.value.manufacturer))))
+            },
+            2 => {
+                self.state += 1;
+                Ok(Some(try!(serializer.serialize_struct_elt("price", &self.value.price))))
+            },
+            3 => {
+                self.state += 1;
+                Ok(Some(try!(serializer.serialize_struct_elt("summary", &self.value.summary))))
+            },
+            _ => {
+                Ok(None)
+            }
         }
     }
 }
