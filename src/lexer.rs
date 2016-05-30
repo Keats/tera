8bc6750d76dd6ea426c170f647f038c2103274e5
Alessandro Pellizzari <alex@amiran.it>
2016-05-30 08:05:48 +0100

    Reverted \n detection after comments.

--- src/lexer.rs
+++ src/lexer.rs
@@ -304,19 +304,7 @@ impl Lexer {
                     _ => StateFn(Some(lex_inside_block))
                 }
             },
-            DelimiterSide::Right => {
-                match self.current_block_type {
-                    BlockType::Comment => {
-                        if !self.is_over() && self.peek() == '\n' {
-                            self.start += 1;
-                            self.current_char += 1;
-                            self.position += 1;
-                        }
-                    },
-                    _ => {}
-                };
-                StateFn(Some(lex_text))
-            }
+            DelimiterSide::Right => StateFn(Some(lex_text)),
         }
     }
 }
