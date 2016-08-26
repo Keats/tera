868d133398d15e57919219a3909e29bd13c92d9e
Vincent Prouillet <vincent@wearewizards.io>
2016-08-26 14:04:13 +0100

    Explain try_get_value macro a bit more

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,7 @@
 /// Helper macro to get real values out of Value while retaining
 /// proper errors
+/// Takes 4 args: filter name, variable name (use `value` if it's the value the filter
+/// is ran on), the expected type and the actual variable
 #[macro_export]
 macro_rules! try_get_value {
     ($filter_name:expr, $var_name:expr, $ty:ty, $val:expr) => {{
