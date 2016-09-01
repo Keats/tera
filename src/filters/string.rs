6616a16084cc883a42a226692cdb17251f7a54c5
orhanbalci <orhanbalci@gmail.com>
2016-09-01 15:00:44 +0300

    replace filter implemented

--- src/filters/string.rs
+++ src/filters/string.rs
@@ -53,6 +53,57 @@ pub fn wordcount(value: Value, _: HashMap<String, Value>) -> TeraResult<Value> {
     Ok(to_value(&s.split_whitespace().count()))
 }
 
+/// Replaces given number of substrings with  new ones. If count is not given replaces all
+/// occurrences
+pub fn replace(value: Value, args: HashMap<String, Value>) -> TeraResult<Value> {
+    let mut s = try_get_value!("replace", "value", String, value);
+
+    let count = match args.get("count") {
+        Some(c) => try_get_value!("replace", "count", i32, c.clone()),
+        None => -1
+    };
+
+    let old_chunk = match args.get("old") {
+        Some(old) => try_get_value!("replace", "old", String, old.clone()),
+        None => String::new() 
+    };
+    
+    let new_chunk = match args.get("new") {
+        Some(new) => try_get_value!("replace", "new", String, new.clone()),
+        None => String::new()
+    };
+
+    // replace all 
+    if count == -1 {
+        s = s.replace(&old_chunk, &new_chunk);
+        Ok(to_value(&s))
+    } 
+    // replace with count
+    else {
+        let indices : Vec<_> =  s.match_indices(&old_chunk).collect();
+        let mut index_counter : usize = 0;
+        let replaced = s.char_indices().fold(String::new(), |acc, x | {
+            if (index_counter == 0 && x.0 < indices[index_counter].0) || 
+               (index_counter > 0 && x.0 < indices[index_counter].0 && x.0 >= indices[index_counter-1].0 + old_chunk.len()) || 
+                index_counter >= count as usize {
+                let mut result = acc.clone();
+                result.push(x.1);
+                result
+            }
+           else if x.0 == indices[index_counter].0 {
+                index_counter += 1;
+                let mut result  = acc.clone();
+                result.push_str(&new_chunk);
+                result
+           }
+           else {
+            acc
+           }
+        });
+        Ok(to_value(&replaced))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
@@ -117,5 +168,26 @@ mod tests {
         let result = wordcount(to_value("Joel is a slug"), HashMap::new());
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), to_value(&4));
+    }
+
+    #[test]
+    fn test_replace() {
+        let mut args = HashMap::new();
+        args.insert("old".to_string(), to_value(&"Hello"));
+        args.insert("new".to_string(), to_value(&"Goodbye"));
+        let result = replace(to_value(&"Hello world!"), args);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), to_value("Goodbye world!"));
+    }
+
+    #[test]
+    fn test_replace_with_count() {
+        let mut args = HashMap::new();
+        args.insert("old".to_string(), to_value(&"a"));
+        args.insert("new".to_string(), to_value(&"d'oh, "));
+        args.insert("count".to_string(), to_value(&2));
+        let result = replace(to_value(&"aaaaargh"), args);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), to_value("d'oh, d'oh, aaargh"));
     }
 }
