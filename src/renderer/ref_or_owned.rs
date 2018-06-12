02ef774dc6c93b21c3637bbfe8828c6a029c9c36
Daniel Davidson <dbdavidson@yahoo.com>
2018-06-12 15:15:13 -0500

    Fix unused warning in `ref_or_owned`

--- src/renderer/ref_or_owned.rs
+++ src/renderer/ref_or_owned.rs
@@ -13,18 +13,18 @@ use std::ops::Deref;
 #[derive(Clone, Debug, Serialize)]
 pub enum RefOrOwned<'a, T>
 where
-  T: 'a,
+    T: 'a,
 {
-  /// The reference variant
-  Borrowed {
-    /// The instance borrowed
-    borrow: &'a T,
-  },
-  /// The owned variant
-  Owned {
-    /// The owned instance
-    owned: T,
-  },
+    /// The reference variant
+    Borrowed {
+        /// The instance borrowed
+        borrow: &'a T,
+    },
+    /// The owned variant
+    Owned {
+        /// The owned instance
+        owned: T,
+    },
 }
 
 // --- module impl definitions ---
@@ -32,144 +32,144 @@ where
 /// Implementation for type `RefOrOwned`.
 impl<'a, T> RefOrOwned<'a, T>
 where
-  T: Clone,
+    T: Clone,
 {
-  /// Get reference to value
-  ///
-  ///  * _return_ - Reference to the value
-  ///
-  #[inline]
-  pub fn get(&self) -> &T {
-    match self {
-      RefOrOwned::Borrowed { borrow } => *borrow,
-      RefOrOwned::Owned { owned } => owned,
+    /// Get reference to value
+    ///
+    ///  * _return_ - Reference to the value
+    ///
+    #[inline]
+    pub fn get(&self) -> &T {
+        match self {
+            RefOrOwned::Borrowed { borrow } => *borrow,
+            RefOrOwned::Owned { owned } => owned,
+        }
     }
-  }
 
-  /// Get reference to value with extended lifetime.
-  ///
-  /// Requires stored value to be borrowed. See also `is_borrowed`.
-  ///
-  ///  * _return_ - Reference to the value
-  ///
-  #[inline]
-  pub fn get_ref(&self) -> Option<&'a T> {
-    match self {
-      RefOrOwned::Borrowed { borrow } => Some(borrow),
-      _ => None,
+    /// Get reference to value with extended lifetime.
+    ///
+    /// Requires stored value to be borrowed. See also `is_borrowed`.
+    ///
+    ///  * _return_ - Reference to the value
+    ///
+    #[inline]
+    pub fn get_ref(&self) -> Option<&'a T> {
+        match self {
+            RefOrOwned::Borrowed { borrow } => Some(borrow),
+            _ => None,
+        }
     }
-  }
 
-  /// Determine if stored value is borrowed
-  ///
-  ///  * _return_ - Return true of stored value is borrowed
-  ///
-  #[inline]
-  pub fn is_borrowed(&self) -> bool {
-    match self {
-      RefOrOwned::Borrowed { borrow } => true,
-      _ => false,
+    /// Determine if stored value is borrowed
+    ///
+    ///  * _return_ - Return true if stored value is borrowed
+    ///
+    #[inline]
+    pub fn is_borrowed(&self) -> bool {
+        match self {
+            RefOrOwned::Borrowed { borrow: _ } => true,
+            _ => false,
+        }
     }
-  }
 
-  /// Determine if stored value is owned
-  ///
-  ///  * _return_ - Return true of stored value is owned
-  ///
-  #[inline]
-  pub fn is_owned(&self) -> bool {
-    match self {
-      RefOrOwned::Owned { owned } => true,
-      _ => false,
+    /// Determine if stored value is owned
+    ///
+    ///  * _return_ - Return true if stored value is owned
+    ///
+    #[inline]
+    pub fn is_owned(&self) -> bool {
+        match self {
+            RefOrOwned::Owned { owned: _ } => true,
+            _ => false,
+        }
     }
-  }
 
-  /// Take reference to value
-  ///
-  ///  * _return_ - Reference to the value
-  ///
-  #[inline]
-  pub fn take(&mut self) -> T
-  where
-    T: Default,
-  {
-    match self {
-      RefOrOwned::Borrowed { borrow } => (*borrow).clone(),
-      RefOrOwned::Owned { owned } => ::std::mem::replace(owned, T::default()),
+    /// Take reference to value
+    ///
+    ///  * _return_ - Reference to the value
+    ///
+    #[inline]
+    pub fn take(&mut self) -> T
+    where
+        T: Default,
+    {
+        match self {
+            RefOrOwned::Borrowed { borrow } => (*borrow).clone(),
+            RefOrOwned::Owned { owned } => ::std::mem::replace(owned, T::default()),
+        }
     }
-  }
 
-  /// Create from borrow
-  ///
-  ///  * `borrow` - Borrowed value
-  ///  * _return_ - New wrapper with reference to value
-  ///
-  #[inline]
-  pub fn from_borrow(borrow: &'a T) -> RefOrOwned<'a, T> {
-    RefOrOwned::Borrowed { borrow }
-  }
+    /// Create from borrow
+    ///
+    ///  * `borrow` - Borrowed value
+    ///  * _return_ - New wrapper with reference to value
+    ///
+    #[inline]
+    pub fn from_borrow(borrow: &'a T) -> RefOrOwned<'a, T> {
+        RefOrOwned::Borrowed { borrow }
+    }
 
-  /// Create from owned
-  ///
-  ///  * `owned` - Owned value
-  ///  * _return_ - New wrapper with owned
-  ///
-  #[inline]
-  pub fn from_owned(owned: T) -> RefOrOwned<'a, T> {
-    RefOrOwned::Owned { owned }
-  }
+    /// Create from owned
+    ///
+    ///  * `owned` - Owned value
+    ///  * _return_ - New wrapper with owned
+    ///
+    #[inline]
+    pub fn from_owned(owned: T) -> RefOrOwned<'a, T> {
+        RefOrOwned::Owned { owned }
+    }
 }
 
 /// Implementation of trait `Deref` for type `RefOrOwned<'a, T>`
 impl<'a, T> Deref for RefOrOwned<'a, T>
 where
-  T: Clone,
+    T: Clone,
 {
-  type Target = T;
+    type Target = T;
 
-  /// Method to dereference a value.
-  ///
-  ///  * _return_ - The dereferenced value
-  ///
-  #[inline]
-  fn deref(&self) -> &Self::Target {
-    self.get()
-  }
+    /// Method to dereference a value.
+    ///
+    ///  * _return_ - The dereferenced value
+    ///
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.get()
+    }
 }
 
 /// Test module for ref_or_owned module
 #[cfg(test)]
 mod tests {
-  use super::*;
-  mod ref_or_owned {
-
     use super::*;
+    mod ref_or_owned {
 
-    #[test]
-    fn from_borrow() -> () {
-      #[derive(Clone, Default)]
-      struct T {
-        t: String,
-      }
+        use super::*;
 
-      let t = T { t: "some t".into() };
-      let ref_or_owned = RefOrOwned::from_borrow(&t);
-      assert_eq!(ref_or_owned.get().t, t.t);
+        #[test]
+        fn from_borrow() -> () {
+            #[derive(Clone, Default)]
+            struct T {
+                t: String,
+            }
+
+            let t = T { t: "some t".into() };
+            let ref_or_owned = RefOrOwned::from_borrow(&t);
+            assert_eq!(ref_or_owned.get().t, t.t);
+        }
+
+        #[test]
+        fn from_owned() -> () {
+            #[derive(Clone, Default, PartialEq, Debug)]
+            struct T {
+                t: String,
+            }
+
+            let t = T { t: "some t".into() };
+            let mut ref_or_owned = RefOrOwned::from_owned(t.clone());
+            assert_eq!(ref_or_owned.get().t, t.t);
+            let taken = ref_or_owned.take();
+            assert_eq!(taken, t);
+            assert_eq!(ref_or_owned.take(), T::default());
+        }
     }
-
-    #[test]
-    fn from_owned() -> () {
-      #[derive(Clone, Default, PartialEq, Debug)]
-      struct T {
-        t: String,
-      }
-
-      let t = T { t: "some t".into() };
-      let mut ref_or_owned = RefOrOwned::from_owned(t.clone());
-      assert_eq!(ref_or_owned.get().t, t.t);
-      let taken = ref_or_owned.take();
-      assert_eq!(taken, t);
-      assert_eq!(ref_or_owned.take(), T::default());
-    }
-  }
 }
