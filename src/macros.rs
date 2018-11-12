6f7bf484328bb6404f8c19ac051e7caa8ff7892c
Vincent Prouillet <prouillet.vincent@gmail.com>
2018-11-12 18:16:37 +0100

    Update examples and run them on CI

--- src/macros.rs
+++ src/macros.rs
@@ -19,12 +19,12 @@ macro_rules! try_get_value {
             Ok(s) => s,
             Err(_) => {
                 if $var_name == "value" {
-                    return Err($crate::errors::Error::msg(format!(
+                    return Err($crate::Error::msg(format!(
                         "Filter `{}` was called on an incorrect value: got `{}` but expected a {}",
                         $filter_name, $val, stringify!($ty)
                     )));
                 } else {
-                    return Err($crate::errors::Error::msg(format!(
+                    return Err($crate::Error::msg(format!(
                         "Filter `{}` received an incorrect type for arg `{}`: got `{}` but expected a {}",
                         $filter_name, $var_name, $val, stringify!($ty)
                     )));
