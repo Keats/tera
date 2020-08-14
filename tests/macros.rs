8e8f448526d0ea965fbece2a620d29dfd5c7da81
Konsta Hölttä <sooda@iki.fi>
2020-08-14 15:53:10 +0300

    Test self namespace with inheritance from #532
    Add tests for a condition where:
    - a template is rendered
    - the template extends another
    - the template imports two macros
    - the second import uses the self namespace
    
    The self namespace wasn't recorded correctly as discussed in #532, but
    only when inheriting from a base template. Furthermore, the imported
    template received the imported macro namespace; this wouldn't happen
    without inheritance.

---
+++ tests/macros.rs
@@ -0,0 +1,45 @@
+extern crate tera;
+
+use tera::{Context, Result, Tera};
+
+fn render_tpl(tpl_name: &str) -> Result<String> {
+    let tera = Tera::new("tests/macros/**/*.html").unwrap();
+
+    tera.render(tpl_name, &Context::new())
+}
+
+#[test]
+fn test_extend_and_include1() {
+    // trivial case: inheritance, only one macro import
+    let result = render_tpl("extend_and_include1.html");
+
+    assert_eq!(result.is_err(), false);
+    assert_eq!(result.unwrap(), "hello");
+}
+
+#[test]
+fn test_extend_and_include2() {
+    // inheritance, two macro imports, a and b, a is used, a uses self
+    let result = render_tpl("extend_and_include2.html");
+
+    assert_eq!(result.is_err(), false);
+    assert_eq!(result.unwrap(), "hello");
+}
+
+#[test]
+fn test_extend_and_include3() {
+    // inheritance, two macro imports, b and a, a is used, a uses self
+    let result = render_tpl("extend_and_include3.html");
+
+    assert_eq!(result.is_err(), false);
+    assert_eq!(result.unwrap(), "hello");
+}
+
+#[test]
+fn test_extend_and_include4() {
+    // like test_extend_and_include3 but no inheritance for completeness
+    let result = render_tpl("extend_and_include4.html");
+
+    assert_eq!(result.is_err(), false);
+    assert_eq!(result.unwrap(), "hello");
+}
