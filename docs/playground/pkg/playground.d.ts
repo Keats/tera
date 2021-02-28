18d9308052553b9f1613635cf660112b75c53caf
Vincent Prouillet <balthek@gmail.com>
2021-02-28 21:23:10 +0100

    Commit pkg folder

---
+++ docs/playground/pkg/playground.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} template
+* @param {string} context
+* @returns {string}
+*/
+export function render(template: string, context: string): string;
