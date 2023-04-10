Keats/tera

commit 9479c2801093d856811fda3660e50fb8be9d3f5c

Always canonicalize glob paths passed to Tera (#799)
* Add test for globs starting with "./"

This is a regression test for https://github.com/Keats/tera/issues/574.

* Always canonicalize glob paths passed to Tera

This is to work around an issue in globwalk
(https://github.com/Gilnaa/globwalk/issues/28) due to which paths
starting with `./` or `../` never match anything.

This fixes https://github.com/Keats/tera/issues/574.

src/tera.rs
@@ -111,9 +111,11 @@ impl Tera {
 
     /// Loads all the templates found in the glob that was given to Tera::new
     fn load_from_glob(&mut self) -> Result<()> {
-        if self.glob.is_none() {
-            return Err(Error::msg("Tera can only load from glob if a glob is provided"));
-        }
+        let glob = match &self.glob {
+            Some(g) => g,
+            None => return Err(Error::msg("Tera can only load from glob if a glob is provided")),
+        };
+
         // We want to preserve templates that have been added through
         // Tera::extend so we only keep those
         self.templates = self
@@ -125,14 +127,13 @@ impl Tera {
 
         let mut errors = String::new();
 
-        let dir = self.glob.clone().unwrap();
-        // We clean the filename by removing the dir given
-        // to Tera so users don't have to prefix everytime
-        let mut parent_dir = dir.split_at(dir.find('*').unwrap()).0;
-        // Remove `./` from the glob if used as it would cause an error in strip_prefix
-        if parent_dir.starts_with("./") {
-            parent_dir = &parent_dir[2..];
-        }
+        // Need to canonicalize the glob path because globwalk always returns
+        // an empty list for paths starting with `./` or `../`.
+        // See https://github.com/Keats/tera/issues/574 for the Tera discussion
+        // and https://github.com/Gilnaa/globwalk/issues/28 for the upstream issue.
+        let (parent_dir, glob_end) = glob.split_at(glob.find('*').unwrap());
+        let parent_dir = std::fs::canonicalize(parent_dir).unwrap();
+        let dir = parent_dir.join(glob_end).into_os_string().into_string().unwrap();
 
         // We are parsing all the templates on instantiation
         for entry in glob_builder(&dir)
@@ -1133,6 +1134,36 @@ mod tests {
         File::create(cwd.join("ho.html")).expect("Failed to create a test file");
         let glob = cwd.join("**").join("*.html").into_os_string().into_string().unwrap();
         let tera = Tera::new(&glob).expect("Couldn't build Tera instance");
+        assert_eq!(tera.templates.len(), 2);
+    }
+
+    // Test for https://github.com/Keats/tera/issues/574
+    #[test]
+    fn glob_work_with_paths_starting_with_dots() {
+        use std::path::PathBuf;
+
+        let this_dir = std::env::current_dir()
+            .expect("Could not retrieve the executable's current directory.");
+
+        let scratch_dir = tempfile::Builder::new()
+            .prefix("tera_test_scratchspace")
+            .tempdir_in(&this_dir)
+            .expect(&format!(
+                "Could not create temporary directory for test in current directory ({}).",
+                this_dir.display()
+            ));
+        dbg!(&scratch_dir.path().display());
+
+        File::create(scratch_dir.path().join("hey.html")).expect("Failed to create a test file");
+        File::create(scratch_dir.path().join("ho.html")).expect("Failed to create a test file");
+        let glob = PathBuf::from("./")
+            .join(scratch_dir.path().file_name().unwrap())
+            .join("**")
+            .join("*.html")
+            .into_os_string()
+            .into_string()
+            .unwrap();
+        let tera = Tera::new(&glob).expect("Couldn't build Tera instance.");
         assert_eq!(tera.templates.len(), 2);
     }
 

commit 77bbeccdcaf322a6ccfb38080b0a2ae60e44d740

Return empty list on invalid globs
Regression was introduced in 9479c2801093d856811fda3660e50fb8be9d3f5c.

Since that commit, providing an invalid glob to tera will make it fail
to create an instance, first (in that commit) by making it panic and
since 1f958784b8be7d8ccba443a19c1417f8ff48b329 by making it return an
error.

While returning an error is not entirely unlogical, it doesn't match
what most languages do with invalid globs.

- Bash will by default return an empty set on invalid globs, as the
  `failglob` option is off by default
- Python will likewise return the empty set instead of throwing an
  exception, when doing something like `glob.glob("/dev/null/*")`
- Rust's `globwalk` will also not error, but return an empty set

In fact, we use globwalk in tera and the only reason we panic is by
accident, because `std::fs::canonicalize()` checks the path.

It is better to match other language's glob behaviour, therefore we
resort back to the original path if `canonicalize()` fails.

We should especially restore the previous behaviour, as this caused a
lot of regressions already, including in zola.

Fixes https://github.com/getzola/zola/issues/2150

ref: #819
ref: #820
ref: #799

src/tera.rs
@@ -132,7 +132,13 @@ impl Tera {
         // See https://github.com/Keats/tera/issues/574 for the Tera discussion
         // and https://github.com/Gilnaa/globwalk/issues/28 for the upstream issue.
         let (parent_dir, glob_end) = glob.split_at(glob.find('*').unwrap());
-        let parent_dir = std::fs::canonicalize(parent_dir)?;
+        let parent_dir = match std::fs::canonicalize(parent_dir) {
+            Ok(d) => d,
+            // If canonicalize fails, just abort it and resume with the given path.
+            // Consumers expect invalid globs to just return the empty set instead of failing.
+            // See https://github.com/Keats/tera/issues/819#issuecomment-1480392230
+            Err(_) => std::path::PathBuf::from(parent_dir),
+        };
         let dir = parent_dir.join(glob_end).into_os_string().into_string().unwrap();
 
         // We are parsing all the templates on instantiation
@@ -1222,9 +1228,10 @@ mod tests {
 
     // https://github.com/Keats/tera/issues/819
     #[test]
-    fn doesnt_panic_on_invalid_glob() {
+    fn empty_list_on_invalid_glob() {
         let tera = Tera::new("\\dev/null/*");
         println!("{:?}", tera);
-        assert!(tera.is_err());
+        assert!(tera.is_ok());
+        assert!(tera.unwrap().templates.is_empty());
     }
 }