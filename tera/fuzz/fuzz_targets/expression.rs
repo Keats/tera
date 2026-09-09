a1ba0272fda714eab06a37be9363f79b15ac6bc5
Vincent Prouillet <github@vincentprouillet.com>
2026-09-09 20:56:24 +0200

    Error on expressions too complex (#1040)
    Closes #1035

--- tera/fuzz/fuzz_targets/expression.rs
+++ tera/fuzz/fuzz_targets/expression.rs
@@ -8,7 +8,14 @@ use tera::Tera;
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = String::from_utf8(data.to_vec()){
         let val = format!("{{{{ {} }}}}", s);
-        let mut tera = Tera::default();
-        let _ = tera.add_raw_templates(vec![("fuzzing.html", val)]);
+        std::thread::Builder::new()
+            .stack_size(2048 * 1024)
+            .spawn(move || {
+                let mut tera = Tera::default();
+                let _ = tera.add_raw_templates(vec![("fuzzing.html", val)]);
+            })
+            .unwrap()
+            .join()
+            .unwrap();
     }
 });
